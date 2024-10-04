5a33042085ae4d9cb9660a341f03d0aa0bf4c3e3
Cezary Lulkowski <lulkowski.cezary@gmail.com>
2024-10-04 13:40:05 +0200

    ok

--- kalkulator.py
+++ kalkulator.py
@@ -1,1 +1,46 @@
-hi
+#kalkulator
+
+while True:
+    x=float(input('Wprowadz liczbe x: '))
+    y=float(input('Wprowadz liczbe y: '))
+
+    dzialanie = input("wybierz dzialanie  + - * /")
+
+    while True:
+        if dzialanie == '+':
+
+            wynik = x + y
+            print(wynik)
+            break
+
+        elif dzialanie == '-':
+            wynik = x - y
+            print(wynik)
+            break
+        elif dzialanie == '*':
+            wynik = x * y
+            print(wynik)
+            break
+        elif dzialanie == '/':
+            if y==0:
+                print("nie dziel przez zero")
+                y=float(input('wstaw poprawna liczbe y: '))
+
+            else:
+                wynik = x / y
+                print(wynik)
+                break
+        else:
+            dzialanie=input("wybierz poprawne dzialanie")
+            continue
+
+
+    czy_ponownie_skorzystac=input("czy chcesz skorzystac z kallkulatora jeszcze raz? t/n")
+    if czy_ponownie_skorzystac == 't':
+        continue
+    else:
+        break
+
+
+
+
